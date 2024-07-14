e67e452e807001ea8ca4d20400d5a5673f0e12b0
Arieffrosta <144914376+Arieffrosta@users.noreply.github.com>
2024-07-14 10:57:56 +0700

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiRepl: 'https://fd08444c-5dac-4e92-a946-188a22986355-00-2k7sc3yqg7h0k.picard.replit.dev'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiRepl: 'https://fd08444c-5dac-4e92-a946-188a22986355-00-2k7sc3yqg7h0k.picard.replit.dev'
 };
 
 /*
